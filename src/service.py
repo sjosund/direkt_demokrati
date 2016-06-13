81105e5f457e0075c32ef70465c68676fff2d5f8
Lars Lowe Sjösund <lassolass@gmail.com>
2016-06-13 18:05:18 +0900

    adds flask service that fetches data from riksdagen

---
+++ src/service.py
@@ -0,0 +1,28 @@
+from datetime import date, timedelta
+import json
+
+from flask import Flask
+
+from riksdagen_data import fetch_propositions
+
+
+app = Flask(__name__)
+
+
+@app.route('/propositions', methods=['GET'])
+def propositions():
+    return json.dumps(fetch_propositions(
+        start=date.today() - timedelta(days=10),
+        end=date.today()
+    ))
+
+
+def main():
+    app.run(
+        host='0.0.0.0',
+        port='5678'
+    )
+
+
+if __name__ == '__main__':
+    main()
