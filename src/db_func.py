b753aaea9f91ee3a3e5f4be129bb5a9751eff608
ahemberg <alexander.hemberg@gmail.com>
2016-06-28 19:30:41 +0900

    Fixed proposition vote functions
    Vote functions:
    vote_for_prop()
    add_vote_for_prop()
    update_vote_for_prop()
    
    Adds a vote, or changes old vote if user has already voted. There is a (potentially) serious bug, mentioned in todo-comments

--- src/db_func.py
+++ src/db_func.py
@@ -191,24 +191,53 @@ def get_proposition_by_criteria(criteria={}):
 def get_proposition_by_date(start,stop):
     return False
 
+#TODO: Proposition vote functions work. But there is a bug: If a user updates a vote with the same value, the up/down-
+#TODO: vote data will be corrupt. Entry in table 'votes' is ok, but the function always increments/decrements when a
+#TODO: user votes. Example: if prop X has 5 upvotes and 3 downvotes and a user downvotes the prop will have 4u 4d. If
+#TODO: the user now downvotes again the prop will have 3u 5d, then 2u 6d and so on. The sum upvote+downvote will always
+#TODO: be correct (in ex 8 votes), but the difference upvote-|downvote| will be skewed. Can this be fixed with SQL???
+#TODO: otherwise we need to implement function has_voted(user_id, prop_id), which will cost us some extra db-calls :/
 
 def vote_for_prop(user_id, prop_id, vote):
     """
-    Used when a user votes for a proposition. Updates two tables: Adds or modifies a row in table ´votes´ (registers
-    a vote) and adds
+    Used to vote for propositions. Adds or modifies a row in table ´votes´ (registers
+    a vote) and adds an upvote or a downvote for each proposition.
 
-    :param user_id:
-    :param prop_id:
-    :param vote:
-    :return:
+    :param user_id: Users uique id
+    :param prop_id: Proposition unique id
+    :param vote: Users vote. +1 for upvote, -1 for downvote
+    :return: True if success, False otherwise
     """
-
-    # TODO: Handle cases for when user has voted. Now it will only return false.
-
-    con = connect_to_db()
 
     if vote not in {1, -1}:
         raise IncorrectVoteException('Incorrect vote value')
+
+    retval = add_vote_for_prop(user_id, prop_id, vote)
+
+    if retval == 1:
+        return True
+    elif retval == 2:
+        # TODO: To fix bug mentioned above, find out how user has voted, only update if new vote differs.
+        if update_vote_for_prop(user_id, prop_id, vote):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def add_vote_for_prop(user_id, prop_id, vote):
+    """
+    Used when a user votes for a proposition. Updates two tables: Adds or modifies a row in table ´votes´ (registers
+    a vote) and adds an upvote or a downvote for each proposition.
+
+    :param user_id: User who votes
+    :param prop_id: Proposition to vote for
+    :param vote: -1 for downvote, +1 for upvote
+    :return: 1 if voting succeded, 2 if user has already voted, 0 if the connection failed.
+    """
+
+    con = connect_to_db()
 
     if con:
         with con:
@@ -225,18 +254,72 @@ def vote_for_prop(user_id, prop_id, vote):
             try:
                 cursor.execute(vote_stmt, (prop_id, user_id, vote, curr_time))
                 cursor.execute(inc_stmt,(prop_id))
+            except Exception as e:
+                #TODO: Better practice might be: sql.err.IntegrityError as e??
+                con.rollback()
+
+                if e.args[0] == 1062:
+                    print('Duplicate entry!')
+                    return 2
+                else:
+                    print('Unknown Error')
+                    return 0
+            else:
+                con.commit()
+                return 1
+
+
+def update_vote_for_prop(user_id, prop_id, vote):
+    """
+    Changes a users vote if they have already voted.
+
+    :param user_id: Users unique id
+    :param prop_id: proposition id
+    :param vote: Vote, +1 for upvote, -1 for downvote
+    :return: True if update succeded, False Otherwise
+    """
+
+    con = connect_to_db()
+
+    if con:
+        with con:
+            cursor = con.cursor()
+            curr_time = int(time.time())
+
+            vote_stmt = "UPDATE votes SET vote = %s, timestamp = %s WHERE user_id = %s AND proposition_id = %s"
+
+            if vote == 1:
+                inc_stmt = "UPDATE " \
+                            "propositions " \
+                           "SET " \
+                            "upvotes = upvotes + 1, downvotes = downvotes - 1 " \
+                           "WHERE " \
+                            "id = %s"
+            elif vote == -1:
+                inc_stmt = "UPDATE " \
+                            "propositions " \
+                           "SET " \
+                            "downvotes = downvotes + 1, upvotes = upvotes - 1 " \
+                           "WHERE " \
+                            "id = %s"
+
+            try:
+                cursor.execute(vote_stmt, (vote, curr_time, user_id, prop_id))
+                cursor.execute(inc_stmt, (prop_id))
             except:
+                print('change fail')
+                print(inc_stmt)
                 con.rollback()
                 return False
             else:
                 con.commit()
                 return True
 
+def has_voted():
+    return False
 
 class IncorrectVoteException(Exception):
     def __init__(self, message):
         super(IncorrectVoteException, self).__init__(message)
 
 
-def has_voted():
-    return False
